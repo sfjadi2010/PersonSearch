7c154ab495ba773e4e158a5168c89a8651645edc
Salman Farsi <salman@acupera.com>
2019-03-15 19:36:38 -0400

    Added Docker support

---
+++ Server/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+run_cmd="dotnet watch -p PearsonSearch run"
+
+dotnet restore
+
+until dotnet ef -s PersonSearch -p PersonSearch database update; do
+>&2 echo "DB is starting up"
+sleep 1
+done
+
+>&2 echo "DB is up - executing command"
+exec $run_cmd
